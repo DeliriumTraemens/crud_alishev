1fd58a5eb406f21c4fd7ef9ac66458e19efa11e1
DeliriumTraemens <oskinnick@gmail.com>
2021-06-04 19:55:11 +0300

    Lesson 25 MySql ver 2 simple JDBC

--- src/main/java/org/nick/crud_alishev/dao/PersonDAO.java
+++ src/main/java/org/nick/crud_alishev/dao/PersonDAO.java
@@ -3,38 +3,55 @@ package org.nick.crud_alishev.dao;
 import org.nick.crud_alishev.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
 	private static int PEOPLE_COUNT;
-	private List<Person> people;
-	{
-		
-		people = new ArrayList<>();
-		people.add(new Person(++PEOPLE_COUNT,"Tom",24,"tom@mail.com"));
-		people.add(new Person(++PEOPLE_COUNT,"Jack",34,"jack@mail.com"));
-		people.add(new Person(++PEOPLE_COUNT,"James",35,"jamesom@mail.com"));
-		people.add(new Person(++PEOPLE_COUNT,"Garry",47,"garry@mail.com"));
-		people.add(new Person(++PEOPLE_COUNT,"Greg",49,"greg@mail.com"));
-	}
 	
-	@Override
-	public String toString() {
-		return "PersonDAO{" +
-				       "people=" + people +
-				       '}';
+	private static final String URL = "jdbc:mysql://localhost:3306/spring_alishev";
+	private static String USERNAME = "root";
+	private static String PASSWORD ="12345";
+	private static Connection connection;
+	static{
+		try {
+			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		}
+		catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
 	}
+
 	
 	public List<Person> index(){
-		return people;
+		List<Person> people = new ArrayList<>();
+		
+//		Создаем переменную для запроса
+		try {
+			Statement statement = connection.createStatement();
+			String SQL = "SELECT * FROM Person";
+			ResultSet resultSet  = statement.executeQuery(SQL);
+			
+			while (resultSet.next()){
+				Person person = new Person();
+				person.setId( resultSet.getInt("id"));
+				person.setName(resultSet.getString("name"));
+				person.setId( resultSet.getInt("age"));
+				person.setName(resultSet.getString("email"));
+			}
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Person show(int id){
-		return people.stream().filter(person->person.getId()==id)/*Условие для фильтра - найти по id*/
-				       .findAny()/*Найти если есть*/
-				       .orElse(null);/*В случае отсутствия соответствия вернуть null*/
+//		return people.stream().filter(person->person.getId()==id)/*Условие для фильтра - найти по id*/
+//				       .findAny()/*Найти если есть*/
+//				       .orElse(null);/*В случае отсутствия соответствия вернуть null*/
 	
 	}
 	
@@ -44,18 +61,18 @@ public class PersonDAO {
 	}
 	
 	public void update(int id, Person updatedPerson) {
-		Person  personToBeUpdated = show(id);
-				personToBeUpdated.setId(id);
-				personToBeUpdated.setName(updatedPerson.getName());
-				personToBeUpdated.setAge(updatedPerson.getAge());
-				personToBeUpdated.setEmail(updatedPerson.getEmail());
-		
-//		people.set(id-=1, personToBeUpdated);
-		System.out.println(people);
+//		Person  personToBeUpdated = show(id);
+//				personToBeUpdated.setId(id);
+//				personToBeUpdated.setName(updatedPerson.getName());
+//				personToBeUpdated.setAge(updatedPerson.getAge());
+//				personToBeUpdated.setEmail(updatedPerson.getEmail());
+//
+////		people.set(id-=1, personToBeUpdated);
+//		System.out.println(people);
 	}
 	
 	public void delete(int id) {
-		people.removeIf(p->p.getId()==id);
+//		people.removeIf(p->p.getId()==id);
 	
 	}
 }
