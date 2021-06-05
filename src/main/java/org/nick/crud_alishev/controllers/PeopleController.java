38a5d025b92df12275bf7e77b13eadb652d3b726
DeliriumTraemens <oskinnick@gmail.com>
2021-06-05 15:17:20 +0300

    Lesson 25 MySql ver 3 simple JDBC index + save

--- src/main/java/org/nick/crud_alishev/controllers/PeopleController.java
+++ src/main/java/org/nick/crud_alishev/controllers/PeopleController.java
@@ -74,14 +74,28 @@ public String newPerson(@ModelAttribute("person") Person person){
 	}
 //	В ModelAttribute принимаем объект person из кавычек и помещаем его в Person person;
 //	В @PathVariable("id") int id принимаем "id" и помещаем его в int id
+//	@PatchMapping("/{id}")
+//	public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
+//		if (bindingResult.hasErrors()){
+//			return "people/edit";
+//		}
+//		personDAO.update(id, person);
+//		return "redirect:/people";
+//	}
+	
+	
 	@PatchMapping("/{id}")
-	public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
+	public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+	                     @PathVariable("id") int id) {
 		if (bindingResult.hasErrors()){
 			return "people/edit";
 		}
+		
+		
 		personDAO.update(id, person);
 		return "redirect:/people";
 	}
+	
 	
 	@DeleteMapping("/{id}")
 	public String delete(@PathVariable("id") int id) {
